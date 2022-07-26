7b9c4f99d4e263cc9bc7bb4b91c5f0854e32ebb9
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-07-26 15:32:21 +0200

    add api routes for posts

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::namespace('Api')
+    ->prefix('posts')
+    ->group(function () {
+        Route::get('/', 'PostController@index');
+        Route::get('/{slug}', 'PostController@show');
+    });
